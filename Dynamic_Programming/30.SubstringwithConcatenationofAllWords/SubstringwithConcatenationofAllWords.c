f7ac906dbfa22913410368c654acfe5750f516be
msreekan <jeanshack@yahoo.com>
2016-06-18 17:23:47 -0700

    Changes to be committed: 	modified:   Dynamic_Programming/30.SubstringwithConcatenationofAllWords/SubstringwithConcatenationofAllWords.c

--- Dynamic_Programming/30.SubstringwithConcatenationofAllWords/SubstringwithConcatenationofAllWords.c
+++ Dynamic_Programming/30.SubstringwithConcatenationofAllWords/SubstringwithConcatenationofAllWords.c
@@ -8,6 +8,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define GET_CHAR_HASH(c) ((c) * 5)
+
+/***********************************************************************/
+/* string_hash: Generate Hash                                          */
+/*                                                                     */
+/***********************************************************************/
+int string_hash(char *str, int wlen)
+{
+    int i, hash = 0;
+    for (i = 0; i < wlen; ++i)
+        hash += GET_CHAR_HASH(str[i]);
+    return hash;
+}
+
 /***********************************************************************/
 /* string_cmp: Compare strings                                         */
 /*                                                                     */
@@ -36,6 +50,15 @@ int str_bin_search(char **ch, int len, char *t, int wlen)
 {
     int s = 0, e = len - 1, mid, cmp;
 
+    /* Ensure the string is within the range of the array */
+    cmp = string_cmp(ch[s], t, wlen);
+    if (cmp > 0)
+        return -1;
+
+    cmp = string_cmp(ch[e], t, wlen);
+    if (cmp < 0)
+        return -1;
+
     /* Binary search loop */
     while (s <= e)
     {
@@ -44,7 +67,7 @@ int str_bin_search(char **ch, int len, char *t, int wlen)
 
         /* Compare the string with the target */
         cmp = string_cmp(ch[mid], t, wlen);
-        if (cmp == 0)
+        if (!cmp)
             return mid;
         else if (cmp > 0)
             e = mid - 1;
@@ -87,7 +110,6 @@ int get_pivot(char **ch, int start, int end, int wlen)
         if (i < j)
             swap_char(&ch[i], &ch[j]);
     }
-
     swap_char(&ch[i], &ch[pivot]);
     return i;
 }
@@ -100,6 +122,7 @@ void quick_sort(char **ch, int start, int end, int wlen)
 {
     int pivot;
 
+    /* Recursion ends */
     if (start >= end)
         return;
 
@@ -122,7 +145,8 @@ int seekSubstring(char* s, int soff, char **words, int wordsSize,
     if (wcount == 0)
         return 1;
 
-    /* End of the main string, return failure */
+    /* If the total length of all the words in the set exceed the,
+    string size, then return failure */
     if ((soff + (wcount * wlen)) > slen)
         return 0;
 
@@ -130,7 +154,7 @@ int seekSubstring(char* s, int soff, char **words, int wordsSize,
     if (m_arr[soff] == -1)
         return 0;
 
-    /* Else if array has a valid value, then pick that */
+    /* If array has a valid value, then pick that */
     else if (m_arr[soff] != -2)
         i = m_arr[soff];
 
@@ -152,7 +176,7 @@ int seekSubstring(char* s, int soff, char **words, int wordsSize,
         if (!w_arr[i])
         {
             /* Seek forward  */
-            for (j = i + 1; j < wordsSize; ++j)
+            for (j = i + 1; (j < wordsSize); ++j)
             {
                 int cmp;
 
@@ -199,6 +223,7 @@ int seekSubstring(char* s, int soff, char **words, int wordsSize,
             /* Recursively invoke the operation on the remaining words */
             ret = seekSubstring(s, soff + wlen, words, wordsSize,
                                 wcount - 1, wlen, slen, m_arr, w_arr);
+
             /* Place it back on */
             w_arr[i] = 1;
         }
@@ -223,7 +248,7 @@ int seekSubstring(char* s, int soff, char **words, int wordsSize,
 /***********************************************************************/
 int* findSubstring(char* s, char** words, int wordsSize, int *returnSize)
 {
-    int wlen, slen, wsize, i = 0, arr_off = 0, j;
+    int wlen, slen, i = 0, arr_off = 0, j, w_hash = 0, s_hash;
     int *ret_arr = NULL, *m_arr, *w_arr;
 
     /* Maintain Sanity */
@@ -234,17 +259,18 @@ int* findSubstring(char* s, char** words, int wordsSize, int *returnSize)
     slen = strlen(s);
     wlen = strlen(words[0]);
 
-    /* Calculate the total length of the word array */
-    wsize = wlen * wordsSize;
-
     /* If the word string is longer than the main string, return */
-    if (slen < wsize)
+    if (slen < (wlen * wordsSize))
         return NULL;
 
     /* Quick sort the strings */
     quick_sort(words, 0, wordsSize - 1, wlen);
 
-    /* Memoization array */
+    /* Generate the total hash */
+    for (j = 0; j < wordsSize; ++j)
+        w_hash += string_hash(words[j], wlen);
+
+    /* Comparison memoization array */
     m_arr = malloc(sizeof(int) * (slen + wordsSize));
     if (!m_arr)
         return NULL;
@@ -256,12 +282,18 @@ int* findSubstring(char* s, char** words, int wordsSize, int *returnSize)
     for (j = 0; j < wordsSize; ++j)
         w_arr[j] = 1;
 
+    /* Generate the string hash */
+    s_hash = string_hash(s, wlen * wordsSize);
+
     /* Loop through the array seeking the sub-strings */
-    while(i < slen)
+    j = 0;
+    i = (wlen * wordsSize) - 1;
+    while (i < slen)
     {
-        /* If there is a hit, then save the offset */
-        if (seekSubstring(s, i, words, wordsSize, wordsSize, wlen,
-                          slen, m_arr, w_arr))
+        /* If hash matches and there is a hit, then save the offset */
+        if ((s_hash == w_hash) &&
+            (seekSubstring(s, j, words, wordsSize, wordsSize, wlen,
+                           slen, m_arr, w_arr)))
         {
             /* Make the necessary dynamic allocation */
             if ((!ret_arr) && ((ret_arr = malloc(sizeof(int) * slen)) == NULL))
@@ -269,13 +301,22 @@ int* findSubstring(char* s, char** words, int wordsSize, int *returnSize)
                 free(m_arr);
                 return NULL;
             }
-            ret_arr[arr_off] = i;
+            ret_arr[arr_off] = j;
             arr_off++;
         }
 
         /* Move on */
-        ++i;
+        do
+        {
+            s_hash -= GET_CHAR_HASH(s[j]);
+            j++;
+            ++i;
+            s_hash += GET_CHAR_HASH(s[i]);
+        } while ((i < slen) && ((s_hash != w_hash) || (m_arr[j] == -1)));
     }
+
+    /* Free the memory */
+    free(m_arr);
 
     /* Set the return size and return */
     *returnSize = arr_off;
@@ -299,12 +340,18 @@ int main()
     char *ch4[] = {"aa","aa","aa"};
     char s5[] = "wordgoodgoodgoodbestword";
     char *ch5[] = {"word","good","best","good"};
+    char s6[] = "ababaab";
+    char *ch6[] = {"ab","ba","ba"};
+    char s7[] = "abaababbaba";
+    char *ch7[] = {"ab","ba","ab","ba"};
     int wlen = sizeof(ch) / sizeof(char *), isize, j;
     int wlen1 = sizeof(ch1) / sizeof(char *);
     int wlen2 = sizeof(ch2) / sizeof(char *);
     int wlen3 = sizeof(ch3) / sizeof(char *);
     int wlen4 = sizeof(ch4) / sizeof(char *);
     int wlen5 = sizeof(ch5) / sizeof(char *);
+    int wlen6 = sizeof(ch6) / sizeof(char *);
+    int wlen7 = sizeof(ch7) / sizeof(char *);
     int *i;
 
     /* Find the substring */
@@ -375,7 +422,28 @@ int main()
         free(i);
     }
 
+    /* Find the substring */
+    i = findSubstring(s6, ch6, wlen6, &isize);
+    if (i)
+    {
+        /* Print and free the array */
+        for (j = 0; j < isize; ++j)
+            printf("%d ", i[j]);
+        printf("\n");
+        free(i);
+    }
+
+    /* Find the substring */
+    i = findSubstring(s7, ch7, wlen7, &isize);
+    if (i)
+    {
+        /* Print and free the array */
+        for (j = 0; j < isize; ++j)
+            printf("%d ", i[j]);
+        printf("\n");
+        free(i);
+    }
+
     /* Return */
     return 0;
 }
-
