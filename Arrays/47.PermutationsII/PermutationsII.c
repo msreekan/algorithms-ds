72b4a0e5050427ea5927085aae048242a03a8a77
msreekan <jeanshack@yahoo.com>
2016-07-16 11:30:46 -0700

    new file:   Arrays/46.Permutations/Permutations.c 	new file:   Arrays/46.Permutations/README.md 	new file:   Arrays/47.PermutationsII/PermutationsII.c 	new file:   Arrays/47.PermutationsII/README.md

---
+++ Arrays/47.PermutationsII/PermutationsII.c
@@ -0,0 +1,174 @@
+/***********************************************************************/
+/* Anyone is free to copy, modify, publish, use, compile, sell,        */
+/* dismantle, dismember or distribute this software, either in source  */
+/* code form or as object code or assembly code, for any purpose,      */
+/* commercial or non-commercial, and by any means.                     */
+/***********************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+/***********************************************************************/
+/* swap_int: Swap integer pointers                                     */
+/*                                                                     */
+/***********************************************************************/
+void swap_int(int *a, int *b)
+{
+    int t = *b;
+    *b = *a;
+    *a = t;
+}
+
+/***********************************************************************/
+/* fact: Calculate factorial                                           */
+/*                                                                     */
+/***********************************************************************/
+int fact(int num)
+{
+    int i, f = 1;
+    for (i = 0; i < num; ++i)
+        f *= (num - i);
+    return f;
+}
+
+/***********************************************************************/
+/* copy_arr: Copy the generated array                                  */
+/*                                                                     */
+/***********************************************************************/
+int copy_arr(int **arr1, int *arr2, int len)
+{
+    int i;
+
+    /* Allocate space for the new permutation */
+    *arr1 = malloc(sizeof(int) * len);
+    if (!*arr1)
+      return -1;
+
+    /* Copy contents */
+    for (i = 0; i < len; ++i)
+        *(*arr1 + i) = arr2[i];
+    return 0;
+}
+
+/***********************************************************************/
+/* genp: Generate permutations                                         */
+/*                                                                     */
+/***********************************************************************/
+int genp(int *a, int len, int poffst, int **arr, int *aoff)
+{
+    int i = poffst;
+
+    /* Finished generation of one sequence */
+    if (poffst == len)
+    {
+        /* Copy permutation array */
+        if (copy_arr(&arr[*aoff], a, len))
+            return -1;
+
+        /* Increment the array offset and return */
+        (*aoff) += 1;
+        return 0;
+    }
+
+    /* Loop generating the sequence for this offset location */
+    while (i < len)
+    {
+        int j, k;
+
+        /* Pick a new value for the location. */
+        swap_int(&a[poffst], &a[i]);
+
+        /* Recursively generate permutations for the remaining
+           locations. If the generation fails then return */
+        if (genp(a, len, poffst + 1, arr, aoff))
+           return -1;
+
+        /* Reverse the change at that index */
+        swap_int(&a[poffst], &a[i]);
+
+        /* Find the next value for the location, avoid picking
+        duplicates by scanning the already picked values. */
+        for (j = i + 1; j < len; ++j)
+        {
+            /* Scan the window */
+            for (k = poffst; k < i + 1; ++k)
+                if (a[j] == a[k])
+                    break;
+            /* If we have a unique value at offset j,
+               then break. */
+            if (k == i + 1)
+                break;
+        }
+
+        /* Assign the new offset */
+        i = j;
+    }
+
+    /* Return success */
+    return 0;
+}
+
+/***********************************************************************/
+/* Given a collection of distinct numbers, return all possible         */
+/* permutations.                                                       */
+/*                                                                     */
+/***********************************************************************/
+int** permuteUnique(int* nums, int numsSize, int* returnSize)
+{
+    int f, n = numsSize;
+    int **arr = NULL;
+    int *a = nums;
+
+    /* Maintain sanity */
+    *returnSize = 0;
+    if (!nums || !numsSize)
+        return NULL;
+
+    /* Allocate the return array */
+    f = fact(n);
+    arr = malloc(sizeof(int *) * f);
+    if (!arr)
+        return NULL;
+
+    /* Does not matter if this fails, return the array anyway */
+    genp(a, n, 0, arr, returnSize);
+    return arr;
+}
+
+/***********************************************************************/
+/* prn_arr: Print array!                                               */
+/*                                                                     */
+/***********************************************************************/
+void prn_arr(int *a, int num)
+{
+    int i;
+    for (i = 0; i < num; ++i)
+        printf("%d,", a[i]);
+    printf("\n");
+}
+
+/***********************************************************************/
+/* main: Entry point!                                                  */
+/*                                                                     */
+/***********************************************************************/
+int main()
+{
+    int a1[] = {1, 2, 3}, num, j;
+    int a2[] = {3, 3, 0, 3};
+    int *a[] = {a1, a2};
+    int len[] = {sizeof(a1) / sizeof(int),
+                 sizeof(a2) / sizeof(int)};
+    int **arr;
+    unsigned int i;
+
+    for (i = 0; i < sizeof(a) / sizeof(int *); ++i)
+    {
+        arr = permuteUnique(a[i], len[i], &num);
+        for (j = 0; j < num; ++j)
+        {
+            prn_arr(arr[j], len[i]);
+            free(arr[j]);
+        }
+        free(arr);
+    }
+    return 0;
+}
---
+++ Arrays/47.PermutationsII/README.md
@@ -0,0 +1,25 @@
+******************************************************************************
+Problem Statement: [Permutations](https://leetcode.com/problems/permutations/)
+******************************************************************************
+Given a collection of numbers that might contain duplicates, return all 
+
+possible unique permutations. 
+
+For example,
+[1,1,2] have the following unique permutations:
+
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+
+
+******************************************************************************
+Notes: 
+******************************************************************************
+Time Complexity : O(n ^ m)
+
+Space Complexity : O(1)
+
+******************************************************************************
