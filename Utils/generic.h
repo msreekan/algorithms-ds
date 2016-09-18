3f2824811241cb70d3bf1f4a1783806394e49672
msreekan <jeanshack@yahoo.com>
2016-09-18 16:31:14 -0700

    new file:   Utils/generic.h

---
+++ Utils/generic.h
@@ -0,0 +1,18 @@
+/***********************************************************************/
+/* Anyone is free to copy, modify, publish, use, compile, sell,        */
+/* dismantle, dismember or distribute this software, either in source  */
+/* code form or as object code or assembly code, for any purpose,      */
+/* commercial or non-commercial, and by any means.                     */
+/***********************************************************************/
+#ifndef __GENERIC_H__
+#define __GENERIC_H__
+
+/********************************************/
+/* Macros                                   */
+/********************************************/
+#define INT_SWAP(a, b) {\
+                        int temp = *(a);\
+                        *(a) = *(b);\
+                        *(b) = temp;\
+                        }
+#endif // __STACK_H__
